3004cc71a7566dfef44bcfeb39df0fe7d5e123df
jkuch612 <jkuch612@gmail.com>
2019-04-16 14:35:11 +0000

    Save Point

--- src/app/features/issues/issues-routing.module.ts
+++ src/app/features/issues/issues-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevelopersComponent } from './containers/developers/developers.component';
+import { OverviewComponent } from './containers/overview/overview.component';
 import { IssuesComponent } from './issues.component';
 
 const routes: Routes = [
   {
     path: 'issues',
-    component: IssuesComponent
+    component: IssuesComponent,
+    children: [
+      {
+        path: 'overview',
+        component: OverviewComponent
+      },
+      {
+        path: 'developers',
+        component: DevelopersComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'overview'
+      }
+    ]
   }];
 
 @NgModule({
